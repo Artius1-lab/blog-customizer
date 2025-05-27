4ef12039405ec628adebf184de8550715862d471
unknown <artius1@rambler.ru>
2025-05-27 07:00:02 +0300

    four commit

--- src/ui/select/Select.tsx
+++ src/ui/select/Select.tsx
@@ -43,7 +43,7 @@ export const Select = (props: SelectProps) => {
 		setIsOpen(false);
 		onChange?.(option);
 	};
-	const handlePlaceHolderClick: MouseEventHandler<HTMLDivElement> = (_event) => {
+	const handlePlaceHolderClick: MouseEventHandler<HTMLDivElement> = (event) => {
 		setIsOpen((isOpen) => !isOpen);
 	};
 
